5654081d192eb37ad4c1833172c2a9826dad26b4
Hardik Patel <emailhardikpatel@gmail.com>
2020-10-27 16:51:21 -0700

    Adds Activity, axios and bootstrap

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,4 +1,9 @@
+import '@babel/polyfill';
+import 'mutationobserver-shim';
 import Vue from 'vue';
+import './plugins/bootstrap-vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 
@@ -8,3 +13,5 @@ new Vue({
   router,
   render: (h) => h(App),
 }).$mount('#app');
+
+Vue.use(VueAxios, axios);
---
+++ my-app/src/views/Activity.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="activity">
+    <ActivityComponent />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ActivityComponent from '@/components/ActivityComponent.vue';
+
+export default {
+  name: 'Activity',
+  components: {
+    ActivityComponent,
+  },
+};
+</script>
